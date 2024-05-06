f1f69da0176d53c568a535312e9415ea8ea98edb
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2024-05-06 20:08:14 +0100

    w-fit in tailwind discoved its very usefull

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -1,6 +1,6 @@
 export default function FileDisplay({ handleAudioReset, file, audio }) {
   return (
-    <main className="flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20 ">
+    <main className="flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20 w-fit max-w-full mx-auto">
       <h1 className="font-semibold text-5xl sm:text-6xl md:text-7xl">
         Your<span className="text-blue-400 bold">File</span>
       </h1>
